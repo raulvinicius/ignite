ba24196b6363ec25f277738730ed08a1b3cbd93d
Raul Vinicius <raul.portifolio@gmail.com>
2017-01-27 02:25:38 -0200

    atualizacao para o ACF 5

--- wp-content/themes/ignite/js/main.js
+++ wp-content/themes/ignite/js/main.js
@@ -49,6 +49,19 @@ $(document).ready(function() {
 
 
 	$( '.telefone' ).mask('(00) 0000 0000', {placeholder: "(__) ____ ____"});
+	//incluindo o nono dígito
+	var maskBehavior = function (val) {
+		return val.replace(/\D/g, '').length === 11 ? '00 00000 0000' : '00 0000 00009';
+	}
+
+	options = {
+		onKeyPress: function(val, e, field, options) {
+			field.mask(maskBehavior.apply({}, arguments), options);
+		},
+		placeholder: "__ ____ ____"
+	};
+	 
+	$('.celular').mask(maskBehavior, options);
 	$( '.data' ).mask('00/00/0000', {placeholder: "__/__/____"});
 
 
